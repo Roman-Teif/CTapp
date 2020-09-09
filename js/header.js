26876854bd567951f2c679759ec2ba3ef6f648d3
Roman Teif <64724329+Roman-Teif@users.noreply.github.com>
2020-09-09 11:06:51 +0300

    Update header.js
    // Advanced sw.js registration is added

--- js/header.js
+++ js/header.js
@@ -1,5 +1,17 @@
-   if('serviceWorker' in navigator) {
+   /* if('serviceWorker' in navigator) {
    navigator.serviceWorker
            .register('sw.js')
            .then(function() { console.log('Service Worker Registered'); });
-   }
+   } */
+// Advanced sw.js registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
